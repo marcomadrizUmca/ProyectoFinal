7c892aaf7aa70aa1a09e29d171549263d221c763
CESAR ENRIQUE GOMEZ CARBALLO <cegoca33@hotmail.com>
2021-12-08 00:11:04 -0600

    Cambios

--- ProyectoFinal/Controllers/LoginController.cs
+++ ProyectoFinal/Controllers/LoginController.cs
@@ -7,98 +7,21 @@ using ProyectoFinal.Models;
 
 namespace ProyectoFinal.Controllers
 {
-
-    bancobdEntities modeloBD = new bancobdEntities();
-    //Acción inicial del programa
-    // GET: InicioSesion
-
-   
-    public ActionResult Autenticar()
+    public class LoginController : Controller
     {
-        return View("Autenticar");
-    }
 
-
-
-    //Acción para loguearse
-    [HttpPost]
-    public ActionResult AccesoInicio()
-    {
-        try
+        bancobdEntities modeloBD = new bancobdEntities();
+        // GET: Login
+        public ActionResult Autenticar()
         {
-            //Validación del modelo
-
-            AccesoModel consulta = null;
-
-            
-            {
-
-                consulta = (from d in BD.sp_AutenticacionUsuario(Modelo.usuario, Modelo.clave)
-                            select new AccesoModel
-                            {
-                                usuario = d.usuario,
-                                clave = d.clave,
-                                ultimoIngreso = d.ultimoIngreso,
-                                NombreCompleto = d.NombreCompleto,
-                                id_tipoUsuario = d.TipoDeUsuario,
-                                id_Cliente = d.idCliente
-                            }
-                            ).FirstOrDefault();
-
-
-                //Variable de sesión
-                Session["Usuario"] = consulta;
-
-
-                if (consulta != null)
-                {
-
-                    if (consulta.id_tipoUsuario == 1)
-                    {
-                        return RedirectToAction("Colaborador", "InicioColaborador");
-                    }
-                    else if (consulta.id_tipoUsuario == 2)
-                    {
-                        return RedirectToAction("ClienteInicio", "InicioCliente");
-                    }
-
-                }
-                else
-                {
-                    ViewBag.Mensaje = "El usuario o contraseña son incorrectos";
-                }
-                return View("Autenticacion", Modelo);
-            }
+            return View("Autenticar");
         }
-        catch (Exception)
+
+
+        [HttpPost]
+        public ActionResult Autenticar(sp)
         {
-            return View("_Error");
+            return View("Autenticar");
         }
     }
-    //Final de acción para el tipo de usuario
-
-
-    //Acción para salir del sistema
-    [ValidarSesionFilter]
-    [HttpGet]
-    public ActionResult Salir()
-    {
-        try
-        {
-            Session.RemoveAll();
-            return RedirectToAction("Autenticacion", "InicioSesion");
-        }
-        catch (Exception)
-        {
-
-            return View("_Error");
-        }
-
-
-    }
-
-    //Final de acción para salir del sistema
-
 }
-}
-     
