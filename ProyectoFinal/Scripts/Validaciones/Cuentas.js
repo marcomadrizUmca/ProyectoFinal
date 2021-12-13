f7f9cddd42cb1745788b0c5406d70e8747547430
MARCO MADRIZ HERRERA <madriz.m92@gmail.com>
2021-12-13 10:36:50 -0600

    Cambios Varios

---
+++ ProyectoFinal/Scripts/Validaciones/Cuentas.js
@@ -0,0 +1,54 @@
+﻿///document on ready del view Registro de Personas
+$(function () {
+    creaValidaciones();
+});
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmNuevaCuenta").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+
+            Numero_Cuenta: {
+                required: true,
+                Number: true,
+            },
+
+            Codigo: {
+                required: true,
+                minlength: 8
+
+            },
+
+            Estado: {
+                required: true
+            }
+        }
+    });
+}
+
+function creaValidaciones() {
+    $("#frmModificaCuenta").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+
+            Numero_Cuenta: {
+                required: true,
+                Number: true,
+            },
+
+            Codigo: {
+                required: true,
+                minlength: 8
+
+            },
+
+            Estado: {
+                required:true
+            }
+
+        }
+    });
+}
---
+++ ProyectoFinal/Scripts/Validaciones/TipoCuentas.js
@@ -0,0 +1,44 @@
+﻿///document on ready del view Registro de Personas
+$(function () {
+    creaValidaciones();
+});
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmNuevoTipoCuenta").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+
+            Nombre: {
+                required: true
+            },
+
+            Codigo: {
+                required: true,
+                minlength: 8
+
+            }
+        }
+    });
+}
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmModificaTipoCuentas").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+
+            Nombre: {
+                required: true
+            },
+
+            Codigo: {
+                required: true,
+                minlength: 8
+
+            }
+        }
+    });
+}
